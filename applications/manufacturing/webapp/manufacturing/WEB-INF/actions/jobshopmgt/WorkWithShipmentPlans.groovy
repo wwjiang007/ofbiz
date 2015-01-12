wwjiang007/ofbiz

commit 3b5f78e78e4053316772fcc721e0120591e39840

DEPRECATION: applications/manufacturing: findByAnd variants replaced with findByAnd that takes a boolean useCache parameter.
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1338408 13f79535-47bb-0310-9956-ffa450edef68

applications/manufacturing/webapp/manufacturing/WEB-INF/actions/jobshopmgt/WorkWithShipmentPlans.groovy
@@ -25,7 +25,7 @@ import org.ofbiz.manufacturing.jobshopmgt.ProductionRun;
 shipmentPlans = [];
 rows = [];
 if (shipment && shipment.shipmentId) {
-    shipmentPlans = delegator.findByAnd("OrderShipment", [shipmentId : shipment.shipmentId]);
+    shipmentPlans = delegator.findByAnd("OrderShipment", [shipmentId : shipment.shipmentId], null, false);
 }
 if (shipmentPlans) {
     workInProgress = "false";
@@ -67,7 +67,7 @@ if (shipmentPlans) {
         // Total quantity planned not issued
         plannedQuantity = 0.0;
         qtyPlannedInShipment = [:];
-        plans = delegator.findByAnd("OrderShipment", [orderId : orderItem.orderId ,orderItemSeqId : orderItem.orderItemSeqId]);
+        plans = delegator.findByAnd("OrderShipment", [orderId : orderItem.orderId ,orderItemSeqId : orderItem.orderItemSeqId], null, false);
         plans.each { plan ->
             if (plan.quantity) {
                 netPlanQty = plan.quantity;
@@ -147,7 +147,7 @@ if (shipmentPlans) {
         }
         rows.add(oneRow);
         // Select the production runs, if available
-        productionRuns = delegator.findByAnd("WorkOrderItemFulfillment", [orderId : shipmentPlan.orderId, orderItemSeqId : shipmentPlan.orderItemSeqId, shipGroupSeqId : shipmentPlan.shipGroupSeqId],["workEffortId"]); // TODO: add shipmentId
+        productionRuns = delegator.findByAnd("WorkOrderItemFulfillment", [orderId : shipmentPlan.orderId, orderItemSeqId : shipmentPlan.orderItemSeqId, shipGroupSeqId : shipmentPlan.shipGroupSeqId],["workEffortId"], null, false); // TODO: add shipmentId
         if (productionRuns) {
             workInProgress = "true";
             productionRunsId = "";

applications/manufacturing/src/org/ofbiz/manufacturing/bom/BOMHelper.java
@@ -65,8 +65,8 @@ public class BOMHelper {
         // If the date is null, set it to today.
         if (inDate == null) inDate = new Date();
         int maxDepth = 0;
-        List<GenericValue> productNodesList = delegator.findByAndCache("ProductAssoc", 
-                UtilMisc.toMap("productIdTo", productId, "productAssocTypeId", bomType));
+        List<GenericValue> productNodesList = delegator.findByAnd("ProductAssoc", 
+                UtilMisc.toMap("productIdTo", productId, "productAssocTypeId", bomType), null, true);
         productNodesList = EntityUtil.filterByDate(productNodesList, inDate);
         int depth = 0;
         for (GenericValue oneNode : productNodesList) {
@@ -108,8 +108,8 @@ public class BOMHelper {
             productIdKeys = tree.getAllProductsId();
             productIdKeys.add(productIdKey);
         }
-        List<GenericValue> productNodesList = delegator.findByAndCache("ProductAssoc",
-                UtilMisc.toMap("productIdTo", productId, "productAssocTypeId", bomType));
+        List<GenericValue> productNodesList = delegator.findByAnd("ProductAssoc",
+                UtilMisc.toMap("productIdTo", productId, "productAssocTypeId", bomType), null, true);
         productNodesList = EntityUtil.filterByDate(productNodesList, inDate);
         GenericValue duplicatedNode = null;
         Iterator<GenericValue> nodesIterator = productNodesList.iterator();
@@ -135,11 +135,11 @@ public class BOMHelper {
         String shipmentId = request.getParameter("shipmentId");
 
         try {
-        List<GenericValue> shipmentPlans = delegator.findByAnd("OrderShipment", UtilMisc.toMap("shipmentId", shipmentId));
+        List<GenericValue> shipmentPlans = delegator.findByAnd("OrderShipment", UtilMisc.toMap("shipmentId", shipmentId), null, false);
         for(GenericValue shipmentPlan : shipmentPlans) {
             GenericValue orderItem = shipmentPlan.getRelatedOne("OrderItem");
 
-            List<GenericValue> productionRuns = delegator.findByAndCache("WorkOrderItemFulfillment", UtilMisc.toMap("orderId", shipmentPlan.getString("orderId"), "orderItemSeqId", shipmentPlan.getString("orderItemSeqId"), "shipGroupSeqId", shipmentPlan.getString("shipGroupSeqId")));
+            List<GenericValue> productionRuns = delegator.findByAnd("WorkOrderItemFulfillment", UtilMisc.toMap("orderId", shipmentPlan.getString("orderId"), "orderItemSeqId", shipmentPlan.getString("orderItemSeqId"), "shipGroupSeqId", shipmentPlan.getString("shipGroupSeqId")), null, true);
             if (UtilValidate.isNotEmpty(productionRuns)) {
                 Debug.logError("Production Run for order item (" + orderItem.getString("orderId") + "/" + orderItem.getString("orderItemSeqId") + ") not created.", module);
                 continue;

applications/manufacturing/src/org/ofbiz/manufacturing/bom/BOMNode.java
@@ -100,7 +100,7 @@ public class BOMNode {
         List<GenericValue> rows = delegator.findByAnd("ProductAssoc", 
                 UtilMisc.toMap("productId", product.get("productId"),
                         "productAssocTypeId", partBomTypeId),
-                UtilMisc.toList("sequenceNum","productIdTo ASC"));
+                UtilMisc.toList("sequenceNum","productIdTo ASC"), false);
         rows = EntityUtil.filterByDate(rows, inDate);
         if ((UtilValidate.isEmpty(rows)) && substitutedNode != null) {
             // If no child is found and this is a substituted node
@@ -108,7 +108,7 @@ public class BOMNode {
             rows = delegator.findByAnd("ProductAssoc",
                                         UtilMisc.toMap("productId", substitutedNode.getProduct().get("productId"),
                                                        "productAssocTypeId", partBomTypeId),
-                                        UtilMisc.toList("sequenceNum"));
+                                        UtilMisc.toList("sequenceNum"), false);
             rows = EntityUtil.filterByDate(rows, inDate);
         }
         children = FastList.newInstance();
@@ -229,12 +229,12 @@ public class BOMNode {
             List<GenericValue> productPartRules = delegator.findByAnd("ProductManufacturingRule",
                                                     UtilMisc.toMap("productId", productIdForRules,
                                                     "productIdFor", node.get("productId"),
-                                                    "productIdIn", node.get("productIdTo")));
+                                                    "productIdIn", node.get("productIdTo")), null, false);
             if (substitutedNode != null) {
                 productPartRules.addAll(delegator.findByAnd("ProductManufacturingRule",
                                                     UtilMisc.toMap("productId", productIdForRules,
                                                     "productIdFor", substitutedNode.getProduct().getString("productId"),
-                                                    "productIdIn", node.get("productIdTo"))));
+                                                    "productIdIn", node.get("productIdTo")), null, false));
             }
             productPartRules = EntityUtil.filterByDate(productPartRules, inDate);
             newNode = substituteNode(oneChildNode, productFeatures, productPartRules);
@@ -243,11 +243,11 @@ public class BOMNode {
                 // we try to search for a generic link-rule
                 List<GenericValue> genericLinkRules = delegator.findByAnd("ProductManufacturingRule",
                                                         UtilMisc.toMap("productIdFor", node.get("productId"),
-                                                        "productIdIn", node.get("productIdTo")));
+                                                        "productIdIn", node.get("productIdTo")), null, false);
                 if (substitutedNode != null) {
                     genericLinkRules.addAll(delegator.findByAnd("ProductManufacturingRule",
                                                         UtilMisc.toMap("productIdFor", substitutedNode.getProduct().getString("productId"),
-                                                        "productIdIn", node.get("productIdTo"))));
+                                                        "productIdIn", node.get("productIdTo")), null, false));
                 }
                 genericLinkRules = EntityUtil.filterByDate(genericLinkRules, inDate);
                 newNode = substituteNode(oneChildNode, productFeatures, genericLinkRules);
@@ -256,7 +256,7 @@ public class BOMNode {
                     // we try to search for a generic node-rule
                     List<GenericValue> genericNodeRules = delegator.findByAnd("ProductManufacturingRule",
                                                             UtilMisc.toMap("productIdIn", node.get("productIdTo")),
-                                                            UtilMisc.toList("ruleSeqId"));
+                                                            UtilMisc.toList("ruleSeqId"), false);
                     genericNodeRules = EntityUtil.filterByDate(genericNodeRules, inDate);
                     newNode = null;
                     newNode = substituteNode(oneChildNode, productFeatures, genericNodeRules);
@@ -325,7 +325,7 @@ public class BOMNode {
         List<GenericValue> rows = delegator.findByAnd("ProductAssoc",
                                             UtilMisc.toMap("productIdTo", product.get("productId"),
                                                        "productAssocTypeId", partBomTypeId),
-                                            UtilMisc.toList("sequenceNum"));
+                                            UtilMisc.toList("sequenceNum"), false);
         rows = EntityUtil.filterByDate(rows, inDate);
         if ((UtilValidate.isEmpty(rows)) && substitutedNode != null) {
             // If no parent is found and this is a substituted node
@@ -333,7 +333,7 @@ public class BOMNode {
             rows = delegator.findByAnd("ProductAssoc",
                                         UtilMisc.toMap("productIdTo", substitutedNode.getProduct().get("productId"),
                                                        "productAssocTypeId", partBomTypeId),
-                                        UtilMisc.toList("sequenceNum"));
+                                        UtilMisc.toList("sequenceNum"), false);
             rows = EntityUtil.filterByDate(rows, inDate);
         }
         children = FastList.newInstance();

applications/manufacturing/src/org/ofbiz/manufacturing/bom/BOMServices.java
@@ -83,7 +83,7 @@ public class BOMServices {
         List<String> bomTypes = FastList.newInstance();
         if (bomType == null) {
             try {
-                List<GenericValue> bomTypesValues = delegator.findByAnd("ProductAssocType", UtilMisc.toMap("parentTypeId", "PRODUCT_COMPONENT"));
+                List<GenericValue> bomTypesValues = delegator.findByAnd("ProductAssocType", UtilMisc.toMap("parentTypeId", "PRODUCT_COMPONENT"), null, false);
                 for(GenericValue bomTypesValue : bomTypesValues) {
                     bomTypes.add(bomTypesValue.getString("productAssocTypeId"));
                 }
@@ -143,7 +143,7 @@ public class BOMServices {
             // If the product is a variant of a virtual, then the billOfMaterialLevel cannot be
             // lower than the billOfMaterialLevel of the virtual product.
             List<GenericValue> virtualProducts = delegator.findByAnd("ProductAssoc", 
-                    UtilMisc.toMap("productIdTo", productId, "productAssocTypeId", "PRODUCT_VARIANT"));
+                    UtilMisc.toMap("productIdTo", productId, "productAssocTypeId", "PRODUCT_VARIANT"), null, false);
             virtualProducts = EntityUtil.filterByDate(virtualProducts);
             int virtualMaxDepth = 0;
             for(GenericValue oneVirtualProductAssoc : virtualProducts) {
@@ -183,7 +183,7 @@ public class BOMServices {
             }
             if (alsoVariants.booleanValue()) {
                 List<GenericValue> variantProducts = delegator.findByAnd("ProductAssoc", 
-                        UtilMisc.toMap("productId", productId, "productAssocTypeId", "PRODUCT_VARIANT"));
+                        UtilMisc.toMap("productId", productId, "productAssocTypeId", "PRODUCT_VARIANT"), null, false);
                 variantProducts = EntityUtil.filterByDate(variantProducts, true);
                 for(GenericValue oneVariantProductAssoc : variantProducts) {
                     GenericValue variantProduct = delegator.findOne("Product", UtilMisc.toMap("productId", oneVariantProductAssoc.getString("productId")), false);
@@ -474,7 +474,7 @@ public class BOMServices {
         String shipmentId = (String) context.get("shipmentId");
 
         try {
-            List<GenericValue> packages = delegator.findByAnd("ShipmentPackage", UtilMisc.toMap("shipmentId", shipmentId));
+            List<GenericValue> packages = delegator.findByAnd("ShipmentPackage", UtilMisc.toMap("shipmentId", shipmentId), null, false);
             if (!UtilValidate.isEmpty(packages)) {
                 return ServiceUtil.returnError(UtilProperties.getMessage(resource, "ManufacturingBomPackageAlreadyFound", locale));
             }
@@ -484,7 +484,7 @@ public class BOMServices {
         // ShipmentItems are loaded
         List<GenericValue> shipmentItems = null;
         try {
-            shipmentItems = delegator.findByAnd("ShipmentItem", UtilMisc.toMap("shipmentId", shipmentId));
+            shipmentItems = delegator.findByAnd("ShipmentItem", UtilMisc.toMap("shipmentId", shipmentId), null, false);
         } catch (GenericEntityException gee) {
             return ServiceUtil.returnError(UtilProperties.getMessage(resource, "ManufacturingBomErrorLoadingShipmentItems", locale));
         }
@@ -494,7 +494,7 @@ public class BOMServices {
             // Get the OrderShipments
             List<GenericValue> orderShipments = null;
             try {
-                orderShipments = delegator.findByAnd("OrderShipment", UtilMisc.toMap("shipmentId", shipmentId, "shipmentItemSeqId", shipmentItem.getString("shipmentItemSeqId")));
+                orderShipments = delegator.findByAnd("OrderShipment", UtilMisc.toMap("shipmentId", shipmentId, "shipmentItemSeqId", shipmentItem.getString("shipmentItemSeqId")), null, false);
             } catch (GenericEntityException e) {
                 return ServiceUtil.returnError(UtilProperties.getMessage(resource, "ManufacturingPackageConfiguratorError", locale));
             }

applications/manufacturing/src/org/ofbiz/manufacturing/bom/BOMTree.java
@@ -102,7 +102,7 @@ public class BOMTree {
         String productIdForRules = productId;
         // The selected product features are loaded
         List<GenericValue> productFeaturesAppl = delegator.findByAnd("ProductFeatureAppl", 
-                UtilMisc.toMap("productId", productId, "productFeatureApplTypeId", "STANDARD_FEATURE"));
+                UtilMisc.toMap("productId", productId, "productFeatureApplTypeId", "STANDARD_FEATURE"), null, false);
         List<GenericValue> productFeatures = FastList.newInstance();
         GenericValue oneProductFeatureAppl = null;
         for (int i = 0; i < productFeaturesAppl.size(); i++) {
@@ -164,7 +164,7 @@ public class BOMTree {
     private GenericValue manufacturedAsProduct(String productId, Date inDate) throws GenericEntityException {
         List<GenericValue> manufacturedAsProducts = delegator.findByAnd("ProductAssoc",
                                          UtilMisc.toMap("productId", productId,
-                                         "productAssocTypeId", "PRODUCT_MANUFACTURED"));
+                                         "productAssocTypeId", "PRODUCT_MANUFACTURED"), null, false);
         manufacturedAsProducts = EntityUtil.filterByDate(manufacturedAsProducts, inDate);
         GenericValue manufacturedAsProduct = null;
         if (UtilValidate.isNotEmpty(manufacturedAsProducts)) {

applications/manufacturing/src/org/ofbiz/manufacturing/jobshopmgt/ProductionRunHelper.java
@@ -80,14 +80,14 @@ public class ProductionRunHelper {
         List<GenericValue> tasks = delegator.findByAnd("WorkEffort", 
                 UtilMisc.toMap("workEffortParentId", workEffortId,
                         "workEffortTypeId", "PROD_ORDER_TASK",
-                        "workEffortName", taskName));
+                        "workEffortName", taskName), null, false);
         return (UtilValidate.isNotEmpty(tasks));
     }
 
     public static void getLinkedProductionRuns(Delegator delegator, LocalDispatcher dispatcher, String productionRunId, List<ProductionRun> productionRuns)  throws GenericEntityException {
         productionRuns.add(new ProductionRun(productionRunId, delegator, dispatcher));
         List<GenericValue> linkedWorkEfforts = EntityUtil.filterByDate(delegator.findByAnd("WorkEffortAssoc", 
-                UtilMisc.toMap("workEffortIdTo", productionRunId, "workEffortAssocTypeId", "WORK_EFF_PRECEDENCY")));
+                UtilMisc.toMap("workEffortIdTo", productionRunId, "workEffortAssocTypeId", "WORK_EFF_PRECEDENCY"), null, false));
         for (int i = 0; i < linkedWorkEfforts.size(); i++) {
             GenericValue link = linkedWorkEfforts.get(i);
             getLinkedProductionRuns(delegator, dispatcher, link.getString("workEffortIdFrom"), productionRuns);
@@ -95,7 +95,7 @@ public class ProductionRunHelper {
     }
 
     public static String getRootProductionRun(Delegator delegator, String productionRunId)  throws GenericEntityException {
-        List<GenericValue> linkedWorkEfforts = delegator.findByAnd("WorkEffortAssoc", UtilMisc.toMap("workEffortIdFrom", productionRunId, "workEffortAssocTypeId", "WORK_EFF_PRECEDENCY"));
+        List<GenericValue> linkedWorkEfforts = delegator.findByAnd("WorkEffortAssoc", UtilMisc.toMap("workEffortIdFrom", productionRunId, "workEffortAssocTypeId", "WORK_EFF_PRECEDENCY"), null, false);
         GenericValue linkedWorkEffort = EntityUtil.getFirst(linkedWorkEfforts);
         if (linkedWorkEffort != null) {
             productionRunId = getRootProductionRun(delegator, linkedWorkEffort.getString("workEffortIdTo"));

commit 437d7ac506b3eb6c2fe6db995a499dfa36cb856c

A patch from Christian Carlow for "Error when viewing Shipment Plan from Manufacturing" https://issues.apache.org/jira/browse/OFBIZ-5915
This error appears when clicking on a shipment plan from the Manufacturing Shipment Plans:  Error running script at location [component://manufacturing/webapp/manufacturing/WEB-INF/actions/jobshopmgt/WorkWithShipmentPlans.groovy]: java.lang.ClassCastException: java.util.LinkedHashMap cannot be cast to java.lang.String

The error is caused by an invalid call to delegator.findByAnd at line of 150 of WorkWithShipmentPlans.groovy.

To reproduce:
1.  Create an order for an item associated with a ship group
2.  After order creation click "New Shipment" for the ship group
3.  Change the shipment status to "Scheduled'
4.  Add a Shipment Plan
5.  Navigate to Manufacturing -> Shipment Plans
6.  Click "View" for the shipment plan

jleroux: Only R13.07 branch was concerned. The bug was introduced by r1338408 after the R12.04 release. The trunk and R14.12 branch uses correctly the EntityQuery mechanism

git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release13.07@1651145 13f79535-47bb-0310-9956-ffa450edef68

applications/manufacturing/webapp/manufacturing/WEB-INF/actions/jobshopmgt/WorkWithShipmentPlans.groovy
@@ -147,7 +147,7 @@ if (shipmentPlans) {
         }
         rows.add(oneRow);
         // Select the production runs, if available
-        productionRuns = delegator.findByAnd("WorkOrderItemFulfillment", [orderId : shipmentPlan.orderId, orderItemSeqId : shipmentPlan.orderItemSeqId, shipGroupSeqId : shipmentPlan.shipGroupSeqId],["workEffortId"], null, false); // TODO: add shipmentId
+        productionRuns = delegator.findByAnd("WorkOrderItemFulfillment", [orderId : shipmentPlan.orderId, orderItemSeqId : shipmentPlan.orderItemSeqId, shipGroupSeqId : shipmentPlan.shipGroupSeqId],["workEffortId"], false); // TODO: add shipmentId
         if (productionRuns) {
             workInProgress = "true";
             productionRunsId = "";